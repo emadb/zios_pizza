b5b542ce211c7477f7aa148c0913e6c18d5910ce
Emanuele DelBono <emanuele@codiceplastico.com>
2022-03-30 17:15:05 +0200

    refactoring

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZiosPizza.MixProject do
     [
       app: :zios_pizza,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
--- test/modules/kitchen/pizzaiolo_test.exs
+++ test/modules/kitchen/pizzaiolo_test.exs
@@ -1,7 +1,5 @@
 defmodule ZiosPizza.Kitchen.PizzaioloTest do
   use ExUnit.Case
-  alias ZiosPizza.Kitchen.Pizzaiolo
-  alias ZiosPizza.Kitchen.Pooler
 
   describe "Pizzaiolo" do
     test "Test the pizza building process" do
