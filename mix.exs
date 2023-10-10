27d5cc0379fa96ec4871659937c36cb4586aabaf
Emanuele DelBono <emanuele@codiceplastico.com>
2023-10-10 15:45:17 +0200

    update dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ZiosPizza.MixProject do
     [
       app: :zios_pizza,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -53,18 +53,19 @@ defmodule ZiosPizza.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.11"},
-      {:cowboy, "~> 2.8"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, "~> 0.15"},
+      {:plug, "~> 1.14"},
+      {:cowboy, "~> 2.9"},
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.6"},
+      {:ecto_sql, "~> 3.9"},
+      {:postgrex, "~> 0.16"},
       {:poolboy, "~> 1.5"},
-      {:ex_json_schema, "~> 0.7.4"},
-      {:remix, "~> 0.0.2", only: :dev},
-      {:mock, "~> 0.3.0", only: :test},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
+      {:ex_json_schema, "~> 0.9"},
+      # {:remix, "~> 0.0.2", only: :dev},
+      {:remixed_remix, "~> 2.0", only: :dev},
+      {:mock, "~> 0.3", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 end
